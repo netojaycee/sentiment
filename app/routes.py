2da0091d2f02a9bb085504358d1fafca1d0872da
netojaycee <117773169+netojaycee@users.noreply.github.com>
2024-01-24 19:42:51 +0100

    j

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,18 @@
-from flask import jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from app.twitter_analysis import get_twitter_data, perform_sentiment_analysis
+from nltk import word_tokenize
 from app import app
 
+# Assuming your React build folder is named 'build'
+REACT_APP_BUILD_FOLDER = 'dist'
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def serve_react_app():
+    return send_from_directory(REACT_APP_BUILD_FOLDER, 'index.html')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory(REACT_APP_BUILD_FOLDER, filename)
 
 @app.route('/search', methods=['POST'])
 def fetch_twitter_data():
@@ -24,3 +32,6 @@ def fetch_twitter_data():
         sentiment_results.append({'text': result.get('text', ''), 'sentiment': sentiment})
 
     return jsonify(sentiment_results)
+
+if __name__ == '__main__':
+    app.run(debug=True)
